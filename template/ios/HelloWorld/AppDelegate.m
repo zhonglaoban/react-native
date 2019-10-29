561cbbe2e9025f6f6ff3fb98267adb6502be99d8
grabbou <grabbou@gmail.com>
2019-10-29 21:14:40 +0100

    Revert "Add code to enable Flipper in React Native iOS Template"
    This reverts commit 70274f4e9166dbe9da2d04953a95c2909b455d1d.

--- template/ios/HelloWorld/AppDelegate.m
+++ template/ios/HelloWorld/AppDelegate.m
@@ -11,15 +11,6 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
-#if DEBUG
-#import <FlipperKit/FlipperClient.h>
-#import <FlipperKitLayoutPlugin/FlipperKitLayoutPlugin.h>
-#import <FlipperKitUserDefaultsPlugin/FKUserDefaultsPlugin.h>
-#import <FlipperKitNetworkPlugin/FlipperKitNetworkPlugin.h>
-#import <SKIOSNetworkPlugin/SKIOSNetworkAdapter.h>
-#import <FlipperKitReactPlugin/FlipperKitReactPlugin.h>
-#endif
-
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -45,19 +36,6 @@
   return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
 #else
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
-#endif
-}
-
-+ (void) initializeFlipper:(UIApplication *)application
-{
-#if DEBUG
-  FlipperClient *client = [FlipperClient sharedClient];
-  SKDescriptorMapper *layoutDescriptorMapper = [[SKDescriptorMapper alloc] initWithDefaults];
-  [client addPlugin: [[FlipperKitLayoutPlugin alloc] initWithRootNode: application withDescriptorMapper: layoutDescriptorMapper]];
-  [client addPlugin: [[FKUserDefaultsPlugin alloc] initWithSuiteName:nil]];
-  [client addPlugin: [FlipperKitReactPlugin new]];
-  [client addPlugin: [[FlipperKitNetworkPlugin alloc] initWithNetworkAdapter:[SKIOSNetworkAdapter new]]];
-  [client start];
 #endif
 }
 
